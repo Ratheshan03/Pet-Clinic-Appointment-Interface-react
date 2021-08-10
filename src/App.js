02827855ef6505b382deebe86f22a035caaf23ba
Ratheshan03 <lionratheshan@gmail.com>
2021-08-10 21:28:19 +0530

    installations and Search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { BiArchive } from "react-icons/bi";
+import Search from "./components/Search";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Interface</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <BiArchive className="inline-block text-red-400 align-top" />
+        Your Appointments
+      </h1>
+      <Search />
     </div>
   );
 }
