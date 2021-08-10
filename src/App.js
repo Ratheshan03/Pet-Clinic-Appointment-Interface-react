b7d5d407e46b6f81f3f5215dea335052423e8c2d
Ratheshan03 <lionratheshan@gmail.com>
2021-08-10 21:34:22 +0530

    Appointment component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { BiArchive } from "react-icons/bi";
+import { BiCalendar } from "react-icons/bi";
+import AddAppointment from "./components/AddAppointment";
 import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top" />
+      <h1 className="text-5xl mb-5">
+        <BiCalendar className="inline-block text-red-400 align-top" />
         Your Appointments
       </h1>
+      <AddAppointment />
       <Search />
     </div>
   );
